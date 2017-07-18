c1467baa271368cce5a6e197418c4dddc0ee3346
Wiskyt <samy.bouamama@hotmail.fr>
2017-07-18 10:19:13 +0200

    Ajout de momentjs, import et initialisation de l'heure dans la navbar

--- public/js/components/common/navbar.js
+++ public/js/components/common/navbar.js
@@ -1,25 +1,31 @@
+import moment from 'moment'
+
 let navbar = {
-    templateUrl: 'js/components/common/navbar.html',
-    controller: ['UsersService', '$state', function(UsersService, $state) {
-        'use strict'
-        angular.extend(this, {
-            $onInit() {
-                UsersService.getCurrent().then((user) => {
-                    this.user = user
-                }).catch((err) => {
+   templateUrl: 'js/components/common/navbar.html',
+   controller: ['UsersService', '$state', function (UsersService, $state) {
+      'use strict'
 
-                })
-            },
-            disconnect() {
-                UsersService.disconnect().then(() => {
-                    Materialize.toast('Disconnected', 4000, 'toast-warning')
-                    this.user = null
-                    $state.reload()
-                })
-            }
+      this.date = moment();
+      console.log(this.date);
 
-        })
-    }]
+      angular.extend(this, {
+         $onInit() {
+            UsersService.getCurrent().then((user) => {
+               this.user = user
+            }).catch((err) => {
+
+            })
+         },
+         disconnect() {
+            UsersService.disconnect().then(() => {
+               Materialize.toast('Disconnected', 4000, 'toast-warning')
+               this.user = null
+               $state.reload()
+            })
+         }
+
+      })
+   }]
 }
 
 export default navbar
